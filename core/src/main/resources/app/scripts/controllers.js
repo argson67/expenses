49168149946885ad218b7c19cd968157ec688ce0
Ivan Motyashov <imotyash@netapp.com>
2014-09-10 15:11:41 -0700

    Fixed some browser-specific stuff that led to failures in Chrome

--- core/src/main/resources/app/scripts/controllers.js
+++ core/src/main/resources/app/scripts/controllers.js
@@ -4,7 +4,7 @@ String.prototype.capitalize = function() {
   return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-function mkSort($scope, init = null, prefix = null) {
+function mkSort($scope, init, prefix) {
   function withPrefix(str) {
     if (prefix) {
       return prefix + str.capitalize();
--- core/src/main/resources/app/scripts/services.js
+++ core/src/main/resources/app/scripts/services.js
@@ -1,5 +1,26 @@
 'use strict';
 
+Array.prototype.findIndex = function(predicate) {
+  if (this == null) {
+    throw new TypeError('Array.prototype.find called on null or undefined');
+  }
+  if (typeof predicate !== 'function') {
+    throw new TypeError('predicate must be a function');
+  }
+  var list = Object(this);
+  var length = list.length >>> 0;
+  var thisArg = arguments[1];
+  var value;
+
+  for (var i = 0; i < length; i++) {
+    value = list[i];
+    if (predicate.call(thisArg, value, i, list)) {
+      return i;
+    }
+  }
+  return -1;
+};
+
 function mkDebt(service, $http, userSrv, errSrv) {
   service.payDebt = function (debt) {
     if (debt.paid) {
@@ -97,11 +118,11 @@ angular.module('moneyApp')
       $http.get('api/whoami')
       .then(function (response) {
         service.populateUserData(response.data);
-        success();
+        success && success();
       }, function (err) {
         console.log("No login detected: " + err.data);
         service.reset();
-        failure();
+        failure && failure();
       });
     }
 
